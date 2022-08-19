5b5272c534b9549315dc6ccb101d657ede8c80c9
Ayobola25 <adeboyeayobola@gmail.com>
2022-08-18 18:13:38 -0700

    UPDATED

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WindowsFormsApp2;
+
+namespace UnitTestProject1
+{ }
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculatorTest(double length, double breath, double result)
+        {
+            var cal = new Calculator();
+            var area = cal.Area(2, 3);
+            Assert.AreEqual(5, area);
+        }
+        
+      
+
+
+
+
+    }
+       
+
+        
+    
